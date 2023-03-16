00169d8b37ea0c0aca040744ddad4588200e6bc7
Nelsonatuya <nelsonatuya07@gmail.com>
2023-03-16 12:34:36 -0700

    -printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print statement using printf statement
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,);
+return (0);
+}
